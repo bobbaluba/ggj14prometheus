b44ac43209c9d7a869561516eaa30639edcce084
Sindre Næss <nsindre@gmail.com>
2014-01-25 23:15:11 +0100

    now, we have ajjovs

---
+++ Assets/BulletFlight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletFlight : MonoBehaviour {
+	public Vector2 velocity = new Vector2(1,0);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody2D.velocity = velocity.To3D ();
+
+		//gameObject.constantForce.force.Set (xPow, yPow, zPow);
+	
+	}
+}
---
+++ Assets/DestroyOnCollision.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnCollision : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D other){
+		Destroy (gameObject);
+	}
+}
