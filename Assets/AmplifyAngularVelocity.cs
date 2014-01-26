e8430e36d1358668f0dcf3d06651fba2f0cc165c
Johan Klokkhammer Helsing <bobbaluba>
2014-01-26 22:16:57 +1100

    Axe with torque

--- Assets/AmplifyAngularVelocity.cs
+++ Assets/AmplifyAngularVelocity.cs
@@ -2,14 +2,9 @@
 using System.Collections;
 
 public class AmplifyAngularVelocity : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	public float torque = 1;
 	// Update is called once per frame
 	void Update () {
-	
+		rigidbody2D.AddTorqueAtPosition(torque*Mathf.Sign(rigidbody2D.angularVelocity), Vector2.zero); 
 	}
 }
