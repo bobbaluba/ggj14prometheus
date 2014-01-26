7e2bf8c7e75a99395adb5ed5c5500d1a7fba4c56
Johan Klokkhammer Helsing <bobbaluba>
2014-01-26 23:26:07 +1100

    Improved credits with attribution to chris zabriskie

---
+++ Assets/CreditsText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var mesh = GetComponent<TextMesh>();
+		var creditsText = "";
+		//creditsText += "Developers:\n";
+		creditsText += "Sindre Næss\n";
+		creditsText += "Johan Helsing\n";
+		creditsText += "\nMusic:\n";
+		creditsText += "Chris Zabriskie\n";// - \nThe Temperature of the Air on the Bow of the Kaleetan\n";
+		mesh.text = creditsText;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
